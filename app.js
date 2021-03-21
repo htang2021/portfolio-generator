75baa7c0e0800236df2f32575e442f4e9136260d
Hung Tang <hungtang@hotmail.com>
2021-03-20 23:20:23 -0700

    used inquirer to gather user input on projects and write to object array to store the projects

--- app.js
+++ app.js
@@ -1,16 +1,116 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username'
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:'
+        }
+    ]);
+};
+
+const promptProject = portfolioData => {
+    console.log(`
+    =================
+    Add a New Project
+    =================
+    `);
+
+    // If there's no 'objects' array property, create one
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
+
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['Javascript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project. (Required)'
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
+        }
+
+    ])
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+            return promptProject(portfolioData);
+        } else {
+            return portfolioData;
+        }
+    });
+};
+
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+        console.log(portfolioData);
+    });
+    // .then(projectData => {
+    //     portfolioData.projects.push(projectData);
+    //     if (projectData.confirmAddProject) {
+    //         return promptProject(portfolioData);
+    //     } else {
+    //         return portfolioData;
+    //     }
+    // });
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw err;
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
+// const pageHTML = generatePage(name, github);
+
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+
+
+
+// Below is only for reference - no need anymore for the lesson:
 
 // const printProfileData = profileDataArr => {
 //     // This....
