97539cc660ea5dc43008ccefa88041b9d310549a
Hung Tang <hungtang@hotmail.com>
2021-03-27 18:11:23 -0700

    last step of finished-site

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template');
 const inquirer = require('inquirer');
+//const { writeFile, copyFile } = require('node:fs');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -136,13 +137,39 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
-
-        fs.writeFile('./index.html', pageHTML, err => {
-            if (err) throw err;
-        });
-
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
     });
+
+    //     fs.writeFile('./dist/index.html', pageHTML, err => {
+    //         if (err) {
+    //             console.log(err);
+    //             return;
+    //         }
+    //         console.log('Page created!  Checkout index.html in this directory to see it!');
+
+    //         fs.copyFile('./src/style.css', './dist/style.css', err => {
+    //             if (err) {
+    //                 console.log(err);
+    //                 return;
+    //             }
+    //             console.log('Style sheet copied successfully!');
+    //         });
+    //     });
+
+    // });
 
     // .then(projectData => {
     //     portfolioData.projects.push(projectData);
